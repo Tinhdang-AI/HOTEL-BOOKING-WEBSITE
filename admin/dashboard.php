9277fe54c242919fe32e4e0fbf06462627675dde
TINH DANG <dangductinh1103@gmail.com>
2025-06-01 21:34:02 +0700

    update setting 1

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -3,7 +3,6 @@ require('inc/essentials.php');
 adminLogin();
 
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,42 +12,11 @@ adminLogin();
     <?php require('inc/links.php');?>
 </head>
 <body class="bg-white">
-
-    <div class="container-fluid bg-dark text-light p-3 d-flex align-items-center justify-content-between sticky-top">
-        <h3 class="mb-0 h-font">TIDA WEBSITE</h3>
-        <a href="logout.php" class="btn btn-light btn-sm">LOG OUT</a>
-    </div>
-
-    <div class="col-lg-2 bg-dark border-top border-3 border-secondary" id="dashboard-menu">
-        <nav class="navbar navbar-expand-lg navbar-dark">
-            <div class="container-fluid flex-lg-column align-items-stretch">
-                <h4 class="mt-2 text-light" href="#">ADMIN PANEL</h4>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#adminDropdown" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse flex-column align-items-stretch mt-2" id="adminDropdown">
-                    <ul class="nav nav-pills flex-column">
-                        <li class="nav-item">
-                            <a class="nav-link text-white" href="dashboard.php">Dashboard</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white" href="#">Rooms</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white" href="#">Users</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link text-white" href="#">Settings</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    </div>
-
-    <div class="container-fluid">
+    <?php require('inc/header.php');?>
+    <div class="container-fluid" id="main-content">
         <div class="row">
-            <div class="col-lg-10 ms-auto p-4 overflow-hidden">
+            <div class="col-lg-10 ms-auto p-4 overflow-hidden ">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil provident doloribus aspernatur quibusdam expedita laudantium consectetur iste, praesentium rerum nam perspiciatis fuga corrupti inventore unde quaerat aperiam facere nulla ipsa culpa voluptas esse molestias, eum obcaecati. Nemo, eveniet. Laudantium a veniam dolore ut, animi quam autem doloremque, at ratione, error deleniti amet officiis modi quisquam mollitia impedit dolor consequatur laboriosam eum delectus provident ipsam. Possimus impedit, ducimus nostrum nulla nam, perferendis dolor quo culpa suscipit accusamus dolore illum voluptate temporibus est voluptas molestiae voluptatum reprehenderit nesciunt! Unde suscipit illo animi porro aliquid voluptatem debitis, voluptatum iusto sapiente quod rem nesciunt.
             </div>
         </div>
     </div>
